1ad95d803e50abb079a56f49ada84e79e016a43e
Sibei Chen <git@chencraft.com>
2025-10-27 10:15:38 +0800

    Remove unused `@NonNull` import from SecureGitHubApi

--- src/main/java/com/chencraft/api/secure/SecureGitHubApi.java
+++ src/main/java/com/chencraft/api/secure/SecureGitHubApi.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
