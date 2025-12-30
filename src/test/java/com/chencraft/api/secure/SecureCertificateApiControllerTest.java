766e6bdc0023e11cb9e73006741aff30885d3c42
Sibei Chen <git@chencraft.com>
2025-12-30 12:14:06 +0800

    Auto import

--- src/test/java/com/chencraft/api/secure/SecureCertificateApiControllerTest.java
+++ src/test/java/com/chencraft/api/secure/SecureCertificateApiControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
